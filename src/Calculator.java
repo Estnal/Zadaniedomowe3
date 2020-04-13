596129656801677450d68891180a5afb8f703c07
Damian Rogoz <d.rogoz@o2.pl>
2020-04-13 19:27:47 +0200

    Add files

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+
+    boolean isOdd(int a) {
+        return a % 2 != 0;
+    }
+
+    double circleField(double a) {
+        return Math.PI * (a * a);
+    }
+
+    int power(int a) {
+        return a * a;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal1 = new Calculator();
+        System.out.println(cal1.isEven(6));
+        System.out.println(cal1.isOdd(8));
+        System.out.println(cal1.circleField(10));
+        System.out.println(cal1.power(2));
+    }
+}
